7124b8601b8980c28230057db4dc0d24c63f6c2e
bigchou <40347905s@gmail.com>
2020-08-04 13:51:10 +0800

    code

---
+++ main.py
@@ -0,0 +1,83 @@
+import torch
+import torchvision
+import torch.utils.data
+import random
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from torchvision import transforms
+import torchvision.models as models
+import torch
+import numpy as np
+import torch.nn.functional as F
+from torch.utils.data.sampler import WeightedRandomSampler
+from collections import Counter
+
+
+
+
+class BalancedBatchSampler(torch.utils.data.sampler.Sampler):
+    # https://github.com/galatolofederico/pytorch-balanced-batch
+    def __init__(self, dataset, labels=None):
+        self.labels = labels
+        self.dataset = dict()
+        self.balanced_max = 0
+        # Save all the indices for all the classes
+        for idx in range(0, len(dataset)):
+            label = self._get_label(dataset, idx)
+            if label not in self.dataset:
+                self.dataset[label] = list()
+            self.dataset[label].append(idx)
+            self.balanced_max = len(self.dataset[label]) \
+                if len(self.dataset[label]) > self.balanced_max else self.balanced_max
+        
+        # Oversample the classes with fewer elements than the max
+        for label in self.dataset:
+            while len(self.dataset[label]) < self.balanced_max:
+                self.dataset[label].append(random.choice(self.dataset[label]))
+        self.keys = list(self.dataset.keys())
+        self.currentkey = 0
+        self.indices = [-1]*len(self.keys)
+
+    def __iter__(self):
+        while self.indices[self.currentkey] < self.balanced_max - 1:
+            self.indices[self.currentkey] += 1
+            yield self.dataset[self.keys[self.currentkey]][self.indices[self.currentkey]]
+            self.currentkey = (self.currentkey + 1) % len(self.keys)
+        self.indices = [-1]*len(self.keys)
+    
+    def _get_label(self, dataset, idx, labels = None):
+        if self.labels is not None:
+            return self.labels[idx].item()
+        else:
+            # Trying guessing
+            dataset_type = type(dataset)
+            return dataset.imgs[idx][1]
+
+    def __len__(self):
+        return self.balanced_max*len(self.keys)
+
+
+
+dataset = ImageFolder(
+    "val",
+    transforms.Compose([
+        transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406],std=[0.229, 0.224, 0.225])
+    ])
+)
+
+
+train_loader = torch.utils.data.DataLoader(dataset, sampler=BalancedBatchSampler(dataset), batch_size=32)
+
+
+import time
+
+start = time.time()
+for item, label in train_loader:
+    label = label.cpu().numpy()
+    result = np.where(label == 15)
+    print(item[result[0],0,112,112])
+
+print("elapsed:",time.time()-start)
